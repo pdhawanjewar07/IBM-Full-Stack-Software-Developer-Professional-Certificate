03fc22f44aedf9d74373ba94d489f7e26b375b0c
pdhawanjewar07 <pdhawanjewar07@gmail.com>
2023-09-12 11:11:48 +0530

    added course 11 submission

---
+++ course11/CloudAppDevelopment_Capstone/server/djangoapp/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from django.utils.timezone import now
+
+
+# Create your models here.
+
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(null=False, max_length=30)
+
+    def __str__(self):
+        return self.name + "" + self.description
+
+
+class CarModel(models.Model):
+    TYPES = (
+            ("SEDAN", "Sedan"), ("SUV", "SUV"), ("WAGON", "Wagon"), ("LIMOUSINE", "Limousine"), ("BATMOBILE", "Batmobile")
+        )
+
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30)
+    c_type = models.CharField(max_length=30, choices=TYPES)
+    dealer_id = models.IntegerField()
+    year = models.DateField()
+
+    def __str__(self):
+        return "Name: " + self.name + \
+                " Make Name: "+ self.make.name + \
+                " Type: " + self.c_type + \
+                " Dealer ID: " + str(self.dealer_id)+ \
+                " Year: " + str(self.year)
+                
+
+
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
+
+class DealerReview:
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year,sentiment, id):
+        self.dealership=dealership
+        self.name=name
+        self.purchase=purchase
+        self.review=review
+        self.purchase_date=purchase_date
+        self.car_make=car_make
+        self.car_model=car_model
+        self.car_year=car_year
+        self.sentiment=sentiment #Watson NLU service
+        self.id=id
+
+    def __str__(self):
+        return "Review: " + self.review +\
+                " Sentiment: " + self.sentiment
